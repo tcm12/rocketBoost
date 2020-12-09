4e0473bbb0d594157228b8ef1218d5f0b887f67f
tcm12 <tudor_marius_chis@yahoo.com>
2020-12-09 19:13:17 +0200

    basic movement

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocket = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            rocket.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D) == true)
+             {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
